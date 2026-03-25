8ce6e3e7043dfbbfe7121f1dc4852f7344e16f0b
Ragr3n <robin@ragren.com>
2026-03-25 20:46:37 +0100

    Changed from services to module

--- module.nix
+++ module.nix
@@ -21,7 +21,7 @@ let
   netArgs = lib.optionalString (cfg.interfaces != []) "--interfaces ${lib.escapeShellArgs cfg.interfaces}";
   serviceArgs = lib.optionalString (cfg.services != []) "--services ${lib.escapeShellArgs cfg.services}";
 in {
-  options.services.system2mqtt = with lib; {
+  options.module.system2mqtt = with lib; {
     enable = mkEnableOption "System2MQTT MQTT publisher";
 
     package = mkOption {
